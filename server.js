a1383db3af15ea484b1f82fd8258850d0db491cf
Steven Thomson <darccide@protonmail.com>
2019-02-18 10:27:53 +0900

    prepared for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const appConfig = require("./src/config/main-config.js");
 
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //initialize node app
-appConfig.init(app,express);
+appConfig.init(app, express);
 
 // API calls
 const staticRoutes = require("./routes/static");
@@ -18,24 +18,21 @@ const userRoutes = require("./routes/users");
 const listRoutes = require("./routes/lists");
 const itemRoutes = require("./routes/items");
 
+app.use("/", staticRoutes);
+app.use("/", userRoutes);
+app.use("/", listRoutes);
+app.use("/", itemRoutes);
 
-app.use('/',staticRoutes);
-app.use('/',userRoutes);
-app.use('/',listRoutes);
-app.use('/',itemRoutes);
+// Server static assets if in production
 
-
-if (process.env.NODE_ENV === 'production') {
-
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+  // Set static files
+  app.use(express.static("client/build"));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
-
-
