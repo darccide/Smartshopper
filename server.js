34089a0cf080a17b90990e3131f8c7cbb4c261e4
Steven Thomson <darccide@protonmail.com>
2019-02-18 11:04:55 +0900

    prepared for deploy with adding relative path

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // Handle React routing, return all requests to React app
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 app.listen(port, () => console.log(`Server running on port ${port}`));
